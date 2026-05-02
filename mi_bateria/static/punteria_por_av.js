6a5ef2546e1a70400a5914e142198c92a5c536b0
Mateo Rodríguez <mateorodriguez@192.168.1.15>
2026-05-02 17:01:33 -0300

    añadimos btn de eleccion y form de coordenadas a index

---
+++ mi_bateria/static/punteria_por_av.js
@@ -0,0 +1,7 @@
+// Aquí irá la función que se ejecutará cuando el user escoja por av
+//La función estará en el btn en el parámetro onClick
+
+
+const choosenAnguloVigilancia = () => {
+    window.alert('vas a apuntar por Angulo de vigilancia')
+}
---
+++ mi_bateria/static/punteria_por_orientacion.js
@@ -0,0 +1,7 @@
+// Aquí irá la función que se ejecutará cuando el user escoja por orientacion
+//La función estará en el btn en el parámetro onClick
+
+
+const choosenOrientacion = () => {
+    window.alert('vas a apuntar por orientacion')
+}
